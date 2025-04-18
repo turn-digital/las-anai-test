44583a65dca2d1c4c59ae258a36b1f24550f7200
Pēteris <peteris@turn.lv>
2025-04-18 23:30:14 +0300

    correct address for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 
 export default defineConfig({
-  site: "https://http://www.lasiviegli.lv",
+  site: "https://www.lasiviegli.lv",
   integrations: [
     starlight({
       defaultLocale: "root",
