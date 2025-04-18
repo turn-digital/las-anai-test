6035adb22c3d735c64a200b69046cc7979382ec7
Vladyslav <vlad@turn.lv>
2025-04-18 12:45:15 +0300

    delete test line from file add custom css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import starlight from "@astrojs/starlight";
 import sidebar from "./sidebar.config.js";
+
 // https://astro.build/config
+
 export default defineConfig({
   site: "https://http://www.lasiviegli.lv",
-  // base: "/new-docs-3-layer/",
   integrations: [
     starlight({
       title: "Lasi viegli",
       sidebar,
-      customCss: ["./src/styles/custom.css"],
+      customCss: [
+        // Fontsource files for to regular and semi-bold font weights.
+        "@fontsource/barlow/400.css",
+        "@fontsource/barlow/600.css",
+        "./src/styles/custom.css",
+      ],
       social: [
         {
           icon: "facebook",
